f772d70c079251370809f2cca80a81cf61813fc6
Marek Zelinka <mzelinka17@gmail.com>
2024-10-22 15:46:50 +0200

    add missing type

--- src/app/[slug]/not-found.tsx
+++ src/app/[slug]/not-found.tsx
@@ -1,17 +1,15 @@
-import styles from './not-found.module.css'
+import type { Metadata } from "next";
+import styles from "./not-found.module.css";
 
-export const metadata = {
-  title: `404 Not found`,
+export const metadata: Metadata = {
+  title: "404 Not found",
 };
 
 export default function NotFound() {
   return (
     <div className={styles.wrapper}>
       <h1>404 Not Found</h1>
-      <p>
-        This page does not exist. Please check the
-        URL and try again.
-      </p>
+      <p>This page does not exist. Please check the URL and try again.</p>
     </div>
   );
 }
